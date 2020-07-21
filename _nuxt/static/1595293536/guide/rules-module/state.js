427bc9e6f1ac29d697cda2196105d6d62e96ffc7
Abdelrahman Awad <logaretm1@gmail.com>
2020-07-21 03:05:39 +0200

    deploy

---
+++ _nuxt/static/1595293536/guide/rules-module/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__={staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1595293536",layout:"default",error:null,serverRendered:true,routePath:"\u002Fguide\u002Frules-module",config:{algolia:{apiKey:"498c5d264196aacd7606fed9857deb44",indexName:"vee-validate"}}};
